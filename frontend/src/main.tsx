a87c9937b809f2b9bd5fa394e0561c300bc2aa13
Ivanna <chivushka@gmail.com>
2025-06-11 20:35:30 +0300

    fix: Replaced BrowserRouter with HashRouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,5 @@
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '@/shared/styles/global-styles.css';
 import App from '@/modules/app/app.module';
@@ -14,10 +14,10 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
+    <HashRouter>
       <div className="custom-root">
         <App />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   </QueryClientProvider>,
 );
